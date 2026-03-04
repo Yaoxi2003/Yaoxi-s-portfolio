7497664621901c2e60c40e4bd6ebd3a2d2bbd57b
Yaoxi2003 <Y3517199512@gmail.com>
2026-03-04 16:02:47 +1100

    Update image imports and project thumbnails

--- src/assets/images/Img.jsx
+++ src/assets/images/Img.jsx
@@ -1,12 +1,12 @@
 // Desktop images
 import imageProfileDesktop from './desktop/portfile-desktop.png';
 import imageProfileTablet from './desktop/portfile-tablet.png';
-import thumbnailProject1Large from './desktop/thumbnail-project-1-large.webp';
+import thumbnailProject1Large from './desktop/projectLarge.png';
 
 
 // Mobile images
 import imageProfileMobile from './mobile/portfile-mobile.png';
-import thumbnailProject1Small from './mobile/thumbnail-project-1-small.webp';
+import thumbnailProject1Small from './mobile/projectSm.png';
 
 
 const projects = {
@@ -20,4 +20,5 @@ export {
   imageProfileMobile,
   imageProfileTablet,
   projects
+
 }
